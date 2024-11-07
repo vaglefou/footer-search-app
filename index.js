6c6ab80f87ea8a2dd8c1ff166dc03d19aab5968b
vaglefou <vagfou@protonmail.com>
2024-11-08 00:11:06 +0200

    search endpoint created

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const puppeteer = require('puppeteer')
+const axios = require('axios')
+
+const app = express()
+app.use(express.json())
+
+app.post('/api/search', async (req,res) => {
+    const { query } = req.body
+
+    const searchResults = await seachWeb(query)
+
+    const footerData = []
+    for (const url of searchResults) {
+        const companyInfo = await getFooterCompanyInfo(url)
+        footerData.push({url, company: companyInfo})
+    }
+
+    res.json(footerData)
+})
