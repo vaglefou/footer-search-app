617b6aa2be79bcb5cab1479637c89301f42bf4bb
vagfou <vagfou@protonmail.com>
2024-11-08 04:09:47 +0200

    Puppeeter replaced with puppeeter-cluster and request times improved to a few ms

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express')
 const puppeteer = require('puppeteer')
 const axios = require('axios')
+const {Cluster} = require('puppeteer-cluster')
 
 const app = express()
 app.use(express.json())
@@ -45,30 +46,68 @@ async function searchWeb(query) {
 }
 
 async function getFooterCompanyInfo(url) {
-    try {
-        const browser = await puppeteer.launch()
-        const page = await browser.newPage()
-        await page.goto(url, {waitUntil: 'load', timeout: 0})
+    // try {
+    //     const browser = await puppeteer.launch({headless: true})
+    //     const page = await browser.newPage()
+    //     await page.setRequestInterception(true);
+    //         page.on('request', (request) => {
+    //             if (request.resourceType() === 'image' || request.resourceType() === 'stylesheet' || request.resourceType() === 'font') {
+    //                 request.abort();
+    //             } else {
+    //                 request.continue();
+    //             }
+    //         });
+    //     await page.goto(url, { waitUntil: 'load', timeout: 0 });
 
-        const footerText = await page.evaluate(() => {
-            const footer = document.querySelector('footer')
-            return footer ? footer.innerText : ''
-        })
+    //         const footerText = await page.evaluate(() => {
+    //             const footer = document.querySelector('footer')
+    //             return footer ? footer.innerText : ''
+    //         })
 
-        //Phrases searching
-        const keywords = ['Developed by', 'Powered by','Designed by','Made by','Website by','e-shop','eshop']
-        for (const keyword of keywords) {
-            const index = footerText.indexOf(keyword)
-            if (index !== -1) {
-                return footerText.substring(index + keyword.length).split('\n')[0].trim()
+    //     //Phrases searching
+    //     const keywords = ['Developed by', 'Powered by','Designed by','Made by','Website by','e-shop','eshop']
+    //     for (const keyword of keywords) {
+    //         const index = footerText.indexOf(keyword)
+    //         if (index !== -1) {
+    //             return footerText.substring(index + keyword.length).split('\n')[0].trim()
+    //         }
+    //     }
+    //     return "Δεν βρέθηκε όνομα εταιρίας"
+    // } catch (error) {
+    //     console.error(`Σφάλμα στην ανάγνωση του footer για ${url}`, error)
+    //     return null
+    // }
+
+
+    async function getFooterCompanyInfo(url) {
+        const cluster = await Cluster.launch({
+            concurrency: Cluster.CONCURRENCY_CONTEXT,  // Χρησιμοποιεί ταυτόχρονα context, όχι νέες instantiations του browser
+            maxConcurrency: 19, // Καθορίστε τον αριθμό των ταυτόχρονων εργασιών
+        });
+    
+        const result = await cluster.execute(url, async ({ page, data: url }) => {
+            await page.goto(url, { waitUntil: 'load', timeout: 0 });
+    
+            const footerText = await page.evaluate(() => {
+                const footer = document.querySelector('footer');
+                return footer ? footer.innerText : '';
+            });
+    
+            const keywords = ['Developed by', 'Powered by', 'Designed by', 'Made by', 'Website by', 'e-shop', 'eshop'];
+            for (const keyword of keywords) {
+                const index = footerText.indexOf(keyword);
+                if (index !== -1) {
+                    return footerText.substring(index + keyword.length).split('\n')[0].trim();
+                }
             }
-        }
-        return "Δεν βρέθηκε όνομα εταιρίας"
-    } catch (error) {
-        console.error(`Σφάλμα στην ανάγνωση του footer για ${url}`, error)
-        return null
+            return "Δεν βρέθηκε όνομα εταιρίας";
+        });
+    
+        await cluster.close();
+    
+        return result;
     }
+    
 }
-
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log (`Server is running on port ${PORT}`))
